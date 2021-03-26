33bb7fd28a877f0ad06555169fe3cb0fbdc3b21a
Aphescape <alvarojuegospc@gmail.com>
2021-03-26 19:03:27 +0100

    Portfolio 5

---
+++ Assets/Portfolio 5/pruebaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Requiere un collider para detectar OnTriggerEnter
+[RequireComponent(typeof(Collider))]
+public class pruebaScript : MonoBehaviour
+{
+    public Animator animator;
+    public string parametroBoolean;
+    bool b = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        b = !b;
+        animator.SetBool(parametroBoolean, b);
+    }
+}
