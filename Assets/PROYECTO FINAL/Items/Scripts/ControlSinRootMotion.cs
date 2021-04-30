e33297f3fe623a2d1c204d98a524007f7008da66
Aphescape <alvarojuegospc@gmail.com>
2021-04-30 19:35:25 +0200

    Portfolio 8

---
+++ Assets/PROYECTO FINAL/Items/Scripts/ControlSinRootMotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSinRootMotion : MonoBehaviour
+{
+    public CharacterController _controller;
+    public float _speed = 10;
+    public float _rotSpeed = 180;
+    private Vector3 rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        // Lee input horizontal para orientar en eje Y
+        this.rotation = new Vector3(0, H * _rotSpeed * Time.deltaTime, 0);
+        // Lee input vertical para crear traslación en Z
+        Vector3 move = new Vector3(0, 0, V * Time.deltaTime);
+        // Crea el vector de movimiento en la dir. del personaje
+        move = this.transform.TransformDirection(move);
+        // Aplica el movimiento al CHARACTER CONTROLLER
+        _controller.Move(move * _speed);
+        // Aplica la rotación al OBJETO
+        this.transform.Rotate(this.rotation);
+    }
+}
---
+++ Assets/PROYECTO FINAL/Items/Scripts/Mover_IA.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover_IA : MonoBehaviour
+{
+    public Transform goal; //goal = destino
+    UnityEngine.AI.NavMeshAgent agent; //Componente Agent Nav Mesh
+    Animator SWAT_Soldier_Controller; //El componente Animator
+    public Transform MarcaDestino; //Objeto del escenario que marca visualmente el destino
+    public Camera camara; //La cámara sobre la que se calcula la proyección del clic en el escenario 
+    
+    void Start()
+    {   //Asignaciones para simplificar el código
+        agent = GetComponent <UnityEngine.AI.NavMeshAgent>();
+        SWAT_Soldier_Controller = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { //Si se pulsa el botón izquierdo del ratón se reubica el destino en el escenario
+        if (Input.GetMouseButtonDown(0))
+        { // Se traza un rayo desde el punto pulsado en la pantalla hasta el escenario
+            Ray ray = camara.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            { // Si el rayo choca con algo, actualiza el punto de intersección como destino
+                agent.destination = MarcaDestino.position = hit.point;
+                // Un clip de música se reproduce cada vez que se reubica el destino
+                //MarcaDestino.GetComponent<AudioSource>().Play();                      
+            }
+        
+        
+        
+        }
+        // Activar animación de salto si el agente está en una zona de salto
+        if (agent.isOnOffMeshLink) { SWAT_Soldier_Controller.SetTrigger("saltar"); }
+        // Actualizar la animación de locomoción con los parámetros de avance y giro del agente
+        SWAT_Soldier_Controller.SetFloat("horizontal", transform.InverseTransformDirection(agent.velocity).x);
+        SWAT_Soldier_Controller.SetFloat("vertical", transform.InverseTransformDirection(agent.velocity).z);
+
+    }
+}
