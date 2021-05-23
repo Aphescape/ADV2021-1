efed21b4d664d93eb4e3d658486b1a6b32e738eb
Aphescape <alvarojuegospc@gmail.com>
2021-05-23 16:50:39 +0200

    Final Project ADV
    Proyecto final asignatura ADV

---
+++ Assets/PROYECTO FINAL/Items/Scripts/ControlConRootMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlConRootMotion : MonoBehaviour
+{
+    public float speed = 2F;
+    public float speedGiros = 2F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    Animator animator;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        animator.SetFloat("horizontal", H * speedGiros);
+        animator.SetFloat("vertical", V * speed);
+    }
+}
---
+++ Assets/PROYECTO FINAL/Items/Scripts/FinalCutscene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCutscene : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+    }
+    
+
+}
